9a5efd9e673a40479983cd50d5c66a2f347a3eb9
Mobzaaz <83884461+Mobzaaz@users.noreply.github.com>
2021-10-12 09:36:08 +0200

    Update exos2.py

--- exos2.py
+++ exos2.py
@@ -5,7 +5,7 @@ y = int(input("y : "))
 #Ici, on affiche une phrase indiquant les valeurs de bases des variables
 print(f"x is {x} and y is {y}")
 
-#Les lignes 9, 10, et 11, servent à interchanger les valeurs des variables grâce à une variable temporaire (tmp) qui contiendra temporairement la valeur de a
+#Les lignes 9, 10, et 11, servent à interchanger les valeurs des variables grâce à une variable temporaire (tmp) qui contiendra temporairement la valeur de x
 tmp = x
 x = y
 y = tmp
