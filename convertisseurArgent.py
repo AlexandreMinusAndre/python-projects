65c8b5bccd3dd0b26ca9e48bd9403118dd903348
Mobzaaz <83884461+Mobzaaz@users.noreply.github.com>
2021-10-15 09:10:03 +0200

    Delete convertisseurArgent.py

--- convertisseurArgent.py
+++
@@ -1,4 +0,0 @@
-x = float(input("Somme en € : ")) #On demande la somme à convertir
-y = x * 0.84892 #Ici, on multiplie la valeur entrée par l'utilisateur par 0.84892 (la valeur d'un euro en livre sterling)
-
-print(f"{x} en € est égale à {y} livres")
